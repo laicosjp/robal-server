3a330c286d5c6a586393128a48224503d3caffc2
hulk-higakijin <hm385.chejptks@gmail.com>
2022-12-11 16:41:46 +0900

    feat: ルーティングを変更（apiを使わないため）

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  namespace :v1 do
-    mount_devise_token_auth_for "User", at: "auth"
-  end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  
 end
