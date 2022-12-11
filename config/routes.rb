22ac6459038103615e0efddc743d733767bb9d6c
hulk-higakijin <hm385.chejptks@gmail.com>
2022-12-11 18:27:13 +0900

    やはりapiを使うため、ルーティングを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
 end
