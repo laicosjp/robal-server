7d2483678564d25fe3bd52ff431aa773777fc90f
hulk-higakijin <hm385.chejptks@gmail.com>
2022-12-19 01:42:15 +0900

    feat: ユーザーコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  enum role: { ghost: 0, candidate: 1, employer: 2 }
 end
